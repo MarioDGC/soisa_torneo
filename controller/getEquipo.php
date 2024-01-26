a9036b1d52dfb79008facaeaab4163ed9691cf7f
MarioGonzalezC <mdglezc@gmail.com>
2024-01-25 18:01:07 -0600

    simplify teams drop box

--- controller/getEquipo.php
+++ controller/getEquipo.php
@@ -6,45 +6,26 @@
     $data = [];
     $content = "";
 
-    $sqlEquiposA = "SELECT id_equipo, nombre FROM marioswe_torneo_soisa.equipo WHERE categoria = 'A' ORDER BY nombre ASC";
-    $queryEquiposA = mysqli_query($cnx, $sqlEquiposA);
+    $categorita = array('A', 'B', 'V');
 
     $content .= '<option value="">Seleccionar</option>';
-    $content .= '<option value="" disabled>- Categoría A -</option>';
 
-    while ($arrEquiposA = mysqli_fetch_array($queryEquiposA)) {
+    foreach ($categoria as $cat) {
 
-        $id_equipoA = $arrEquiposA['id_equipo'];
-        $nombreA = $arrEquiposA['nombre'];
+        $sqlEquipos = "SELECT id_equipo, nombre FROM marioswe_torneo_soisa.equipo WHERE categoria = '$cat' ORDER BY nombre ASC";
+        $queryEquipos = mysqli_query($cnx, $sqlEquipos);
 
-        $content .= '<option value="'.$id_equipoA.'">'.$nombreA.'</option>';
+        $content .= '<option value="" disabled>- Categoría '.$cat.' -</option>';
+
+        while ($arrEquipos = mysqli_fetch_array($queryEquiposA)) {
+
+            $id_equipo = $arrEquipos['id_equipo'];
+            $nombre = $arrEquipos['nombre'];
+
+            $content .= '<option value="'.$id_equipo.'">'.$nombre.'</option>';
+        }
     }
 
-    $sqlEquiposB = "SELECT id_equipo, nombre FROM marioswe_torneo_soisa.equipo WHERE categoria = 'B' ORDER BY nombre ASC";
-    $queryEquiposB = mysqli_query($cnx, $sqlEquiposB);
-
-    $content .= '<option value="" disabled>- Categoría B -</option>';
-
-    while ($arrEquiposB = mysqli_fetch_array($queryEquiposB)) {
-
-        $id_equipoB = $arrEquiposB['id_equipo'];
-        $nombreB = $arrEquiposB['nombre'];
-
-        $content .= '<option value="'.$id_equipoB.'">'.$nombreB.'</option>';
-    }
-
-    $sqlEquiposV = "SELECT id_equipo, nombre FROM marioswe_torneo_soisa.equipo WHERE categoria = 'V' ORDER BY nombre ASC";
-    $queryEquiposV = mysqli_query($cnx, $sqlEquiposV);
-
-    $content .= '<option value="" disabled>- Categoría VARONIL -</option>';
-
-    while ($arrEquiposV = mysqli_fetch_array($queryEquiposV)) {
-
-        $id_equipoV = $arrEquiposV['id_equipo'];
-        $nombreV = $arrEquiposV['nombre'];
-
-        $content .= '<option value="'.$id_equipoV.'">'.$nombreV.'</option>';
-    }
 
     $data['content'] = $content;
 
